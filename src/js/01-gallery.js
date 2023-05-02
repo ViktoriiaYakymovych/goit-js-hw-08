6e96637a660d812eeb82f07fbe6f50c6193acf2f
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2023-05-02 09:32:09 +0300

    solved task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+import "../css/01-gallery.css";
+import "../css/common.css";
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const listItems = makeMarkup(galleryItems);
+fullList(listItems);
+
+new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+});
+
+function makeMarkup(elements) {
+    return elements.map((el) => {
+        return `<li class="gallery__item">
+        <a href="${el.original}" class="gallery__link"><img class="gallery__image" src="${el.preview}" alt="${el.description}"></a></li>`;
+    }).join("");
+}
+
+function fullList(markup) {
+    galleryList.innerHTML = markup;
+}
