5e970234c41e8f8df2af8691a932b8f7babd1fb4
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2023-05-02 10:39:43 +0300

    solved task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import "../css/common.css";
 
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+const CURRENT_TIME = 'videoplayer-current-time';
+
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME) || 0);
+
+const onTimeUpdate = function () {
+    player.getCurrentTime().then(function (seconds) {
+    localStorage.setItem(CURRENT_TIME, seconds);
+    })
+    // .catch(function(error) {
+    //     alert('Ooops, error!')
+    // });
+};
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
